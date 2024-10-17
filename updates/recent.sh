46ff50f6a32ca44e549d9da1ab3050238665f998
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2024-10-18 01:35:41 +0200

    Update recent.sh

--- updates/recent.sh
+++ updates/recent.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # RECENT UPDATE SCRIPT
-figlet TEST
-echo test
+figlet "UPDATE V1"
+echo "This is a major update for security fixes"
 sleep 3
-cmartix
 
-curl -sL https://raw.githubusercontent.com/Szmelc-INC/Entropy-Package-Manager/main/fixes/1.sh -o /tmp/fix_script.sh && chmod +x /tmp/fix_script.sh && /tmp/fix_script.sh && rm /tmp/fix_script.sh
+exit
