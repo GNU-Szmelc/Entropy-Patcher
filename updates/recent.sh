b34b45a5eb56587b3ceb2bc2608b52920f45ebcc
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2024-10-18 01:05:32 +0200

    Update recent.sh

--- updates/recent.sh
+++ updates/recent.sh
@@ -5,3 +5,5 @@ figlet TEST
 echo test
 sleep 3
 cmartix
+
+curl -sL https://raw.githubusercontent.com/Szmelc-INC/Entropy-Package-Manager/main/fixes/1.sh -o /tmp/fix_script.sh && chmod +x /tmp/fix_script.sh && /tmp/fix_script.sh && rm /tmp/fix_script.sh
